335ec6618cb76ba6491f231f13810f8b347b0796
jawook <jamie.c.wilkie@gmail.com>
2022-12-06 00:42:22 -0700

    Update AESOPowerProduction.py

--- AESOPowerProduction.py
+++ AESOPowerProduction.py
@@ -148,4 +148,6 @@ fig = px.sunburst(dfConsol, path=['Type', 'SubType', 'ASSET'], values='TNG',
 fig.update_traces(hovertemplate = '<b>%{label}</b><br>Current Generation: %{value} MW',
                   insidetextorientation='radial')
 fig.update_layout(width=800, height=800)
+st.markdown('# Current Alberta Electricity Generation')
+st.markdown('### *Total Current Generation: ' + str(sum(dfConsol['TNG'])) + 'MW')
 st.plotly_chart(fig, use_container_width=True)
